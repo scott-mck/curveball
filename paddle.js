52cfbdd4abbc7e75eb9296007e1973b49ef021ea
Scott McKenzie <slammayjammay@gmail.com>
2015-10-30 09:31:06 -0700

    Make mousemove function a paddle prototype

--- paddle.js
+++ paddle.js
@@ -7,17 +7,24 @@
     this.paddleMesh = paddleMesh;
     this.posX = 0;
     this.posY = 0;
+    this.speedX = 0;
+    this.speedY = 0;
 
-    $(document).on('mousemove', function (event) {
-      var canvasBox = renderer.domElement.getBoundingClientRect();
-      var canvasMouseX = event.clientX - canvasBox.left;
-      var canvasMouseY = event.clientY - canvasBox.top;
+    $(document).on('mousemove', this.updateMouse.bind(this));
+  };
 
-      this.posX = (canvasMouseX / renderer.domElement.clientWidth) * 2 - 1;
-      this.posY = -(canvasMouseY / renderer.domElement.clientHeight) * 2 + 1;
+  Paddle.prototype.updateMouse = function (event) {
+    var canvasBox = renderer.domElement.getBoundingClientRect();
+    var canvasMouseX = event.clientX - canvasBox.left;
+    var canvasMouseY = event.clientY - canvasBox.top;
 
-      this.posX *= 34;
-      this.posY *= 21;
-    }.bind(this));
+    var oldX = this.posX;
+    var oldY = this.posY;
+
+    this.posX = (canvasMouseX / renderer.domElement.clientWidth) * 2 - 1;
+    this.posY = -(canvasMouseY / renderer.domElement.clientHeight) * 2 + 1;
+
+    this.posX *= 34;
+    this.posY *= 21;
   };
 })();
