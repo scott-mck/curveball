f0a6d1ae0b142093f85a4c4f3d328676104d5dd6
Scott McKenzie <slammayjammay@gmail.com>
2015-10-30 07:42:49 -0700

    Add method to create collision points

--- ball.js
+++ ball.js
@@ -9,4 +9,21 @@
     this.speedY = .2;
     this.speedZ = -.5;
   }
+
+  Ball.prototype.getCollisionPoints = function () {
+    var points = [ballMesh.position.clone()];
+    var ballPos = ballMesh.position.clone();
+    var angle = 0;
+    for (var i = 0; i < 8; i++) {
+      var x = Math.cos(angle);
+      var y = Math.sin(angle);
+
+      var dir = new THREE.Vector3(x, y, 0);
+      dir.multiplyScalar(radius);
+      points.push(dir.add(ballPos));
+
+      angle += Math.PI / 4;
+    }
+    return points;
+  };
 })();
