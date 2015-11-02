c2a969a9b0641c46942f942bc8988cc09af9cd52
Scott McKenzie <slammayjammay@gmail.com>
2015-11-01 22:58:16 -0800

    Remove unnecessary code

--- paddle.js
+++ paddle.js
@@ -9,17 +9,12 @@
     this.posY = 0;
     this.speedX = 0;
     this.speedY = 0;
-
-    $(document).on('mousemove', this.getMousePos.bind(this));
   };
 
   Paddle.prototype.getMousePos = function (event) {
     var canvasBox = renderer.domElement.getBoundingClientRect();
     var canvasMouseX = event.clientX - canvasBox.left;
     var canvasMouseY = event.clientY - canvasBox.top;
-
-    var oldX = this.posX;
-    var oldY = this.posY;
 
     this.posX = (canvasMouseX / renderer.domElement.clientWidth) * 2 - 1;
     this.posY = -(canvasMouseY / renderer.domElement.clientHeight) * 2 + 1;
