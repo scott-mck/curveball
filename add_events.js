e2e8e03a068797c464d8e83e66500a7541d39062
Scott McKenzie <slammayjammay@gmail.com>
2015-11-03 02:13:43 -0800

    Check for paddle collision in correct direction

--- add_events.js
+++ add_events.js
@@ -8,11 +8,11 @@ addEvents = function () {
   $('#canvas').on('mousedown', function () {
     if (ball.inPlay || ball.dead) return;
 
-    if (game.checkPaddleCollision(-1)) {
+    if (game.checkPaddleCollision(1)) {
       var oldX = playerMesh.position.x;
       var oldY = playerMesh.position.y;
       $('#canvas').one('mouseup', function () {
-        if (game.checkPaddleCollision(-1)) {
+        if (game.checkPaddleCollision(1)) {
           ball.updateSpin(oldX, oldY, playerMesh);
           game.startPlay();
         }
