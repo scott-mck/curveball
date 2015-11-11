c620736e193c91d05b013b990558dd5e4982633f
Scott McKenzie <slammayjammay@gmail.com>
2015-11-10 19:42:33 -0800

    Resize window

--- add_events.js
+++ add_events.js
@@ -1,22 +1,57 @@
 addEvents = function () {
-  game.hideCursor();
+  $(window).load(function () {
+    window.container = $('#canvas');
+    window.resizeId = null;
+    window.canvasSize = .9
+    resizeWindow();
 
-  $(document).on('mousemove', function (event) {
-    player.getMousePos(event);
-  });
+    $(window).resize(function () {
+      clearTimeout(resizeId);
+      resizeId = setTimeout(resizeWindow, 100);
+    });
 
-  $('#canvas').on('mousedown', function () {
-    if (ball.inPlay || ball.dead) return;
+    function resizeWindow () {
+      var width = container.width();
+      var height = container.height();
+      var windowWidth = $(window).width();
+      var windowHeight = $(window).height();
+      var scale;
 
-    if (game.checkPaddleCollision(1)) {
-      var oldX = playerMesh.position.x;
-      var oldY = playerMesh.position.y;
-      $('#canvas').one('mouseup', function () {
-        if (game.checkPaddleCollision(1)) {
-          ball.updateSpin(oldX, oldY, playerMesh);
-          game.startPlay();
-        }
-      });
+      if (windowWidth > windowHeight) {
+        scale = windowWidth / width;
+        if (height * scale > windowHeight) scale = windowHeight / height;
+      } else {
+        scale = windowHeight / height;
+        if (width * scale > windowWidth) scale = windowWidth / width;
+      }
+
+      $('canvas').css('width', width * scale * canvasSize + 'px');
+      $('canvas').css('height', height * scale * canvasSize + 'px');
+
+      camera.aspect = (width * scale) / (height * scale);
+      camera.updateProjectionMatrix();
+      renderer.setSize(width * scale * canvasSize, height * scale * canvasSize);
+      renderer.render(scene, camera);
     }
+
+    $(document).on('mousemove', function (event) {
+      player.getMousePos(event);
+    });
+
+    $('#canvas').on('mousedown', function () {
+      if (ball.inPlay || ball.dead) return;
+
+      if (game.checkPaddleCollision(1)) {
+        var oldX = playerMesh.position.x;
+        var oldY = playerMesh.position.y;
+        $('#canvas').one('mouseup', function () {
+          if (game.checkPaddleCollision(1)) {
+            ball.updateSpin(oldX, oldY, playerMesh);
+            game.startPlay();
+          }
+        });
+      }
+    });
+
   });
 };
