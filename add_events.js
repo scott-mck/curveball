d6f879ca4a1a291df789ddd9e308bb6c72648e1e
Scott McKenzie <slammayjammay@gmail.com>
2015-11-11 20:35:44 -0800

    Refactor

--- add_events.js
+++ add_events.js
@@ -1,57 +1,56 @@
+var container, resizeId, canvasSize;
+
 addEvents = function () {
-  $(window).load(function () {
-    window.container = $('#canvas');
-    window.resizeId = null;
-    window.canvasSize = .9
-    resizeWindow();
+  container = $('#canvas');
+  resizeId = null;
+  canvasSize = .9
+  resizeWindow();
 
-    $(window).resize(function () {
-      clearTimeout(resizeId);
-      resizeId = setTimeout(resizeWindow, 100);
-    });
+  $(window).resize(function () {
+    clearTimeout(resizeId);
+    resizeId = setTimeout(resizeWindow, 100);
+  });
 
-    function resizeWindow () {
-      var width = container.width();
-      var height = container.height();
-      var windowWidth = $(window).width();
-      var windowHeight = $(window).height();
-      var scale;
+  function resizeWindow () {
+    var width = container.width();
+    var height = container.height();
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    var scale;
 
-      if (windowWidth > windowHeight) {
-        scale = windowWidth / width;
-        if (height * scale > windowHeight) scale = windowHeight / height;
-      } else {
-        scale = windowHeight / height;
-        if (width * scale > windowWidth) scale = windowWidth / width;
-      }
-
-      $('#canvas').css('width', width * scale * canvasSize + 'px');
-      $('#canvas').css('height', height * scale * canvasSize + 'px');
-
-      camera.aspect = (width * scale) / (height * scale);
-      camera.updateProjectionMatrix();
-      renderer.setSize(width * scale * canvasSize, height * scale * canvasSize);
-      renderer.render(scene, camera);
+    if (windowWidth > windowHeight) {
+      scale = windowWidth / width;
+      if (height * scale > windowHeight) scale = windowHeight / height;
+    } else {
+      scale = windowHeight / height;
+      if (width * scale > windowWidth) scale = windowWidth / width;
     }
 
-    $(document).on('mousemove', function (event) {
-      player.getMousePos(event);
-    });
+    $('#canvas').css('width', width * scale * canvasSize + 'px');
+    $('#canvas').css('height', height * scale * canvasSize + 'px');
 
-    $('#canvas').on('mousedown', function () {
-      if (ball.inPlay || ball.dead) return;
+    camera.aspect = (width * scale) / (height * scale);
+    camera.updateProjectionMatrix();
+    renderer.setSize(width * scale * canvasSize, height * scale * canvasSize);
+    renderer.render(scene, camera);
+  }
 
-      if (game.checkPaddleCollision(1)) {
-        var oldX = playerMesh.position.x;
-        var oldY = playerMesh.position.y;
-        $('#canvas').one('mouseup', function () {
-          if (game.checkPaddleCollision(1)) {
-            ball.updateSpin(oldX, oldY, playerMesh);
-            game.startPlay();
-          }
-        });
-      }
-    });
+  $(document).on('mousemove', function (event) {
+    player.getMousePos(event);
+  });
 
+  $('#canvas').on('mousedown', function () {
+    if (ball.inPlay || ball.dead) return;
+
+    if (game.checkPaddleCollision(1)) {
+      var oldX = playerMesh.position.x;
+      var oldY = playerMesh.position.y;
+      $('#canvas').one('mouseup', function () {
+        if (game.checkPaddleCollision(1)) {
+          ball.updateSpin(oldX, oldY, playerMesh);
+          game.startPlay();
+        }
+      });
+    }
   });
 };
